5b1d3f7203db847dde536ddd1589507bdd794a11
_sectasy <64311472+sectasy0@users.noreply.github.com>
2020-08-18 15:52:36 +0200

    refactor: better code organization

--- ESPap.ino
+++ ESPap.ino
@@ -1,5 +1,7 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
+#include <ESP8266mDNS.h>;
+
 #include <ESP8266WebServer.h>
 
 const char *ssid = "***** ***";
@@ -7,25 +9,27 @@ const char *password = "wiadomoktowiadomokogowiadomoco";
 
 ESP8266WebServer server(80);
 
-void handleRoot() {
-   server.send(200, "text/html", "<h1>Connected</h1>");
+void runWebServer() {
+  server.on("/",[]() {
+    server.send(200, "text/html", "connected");
+  });
+  
+  server.begin();
+  Serial.println("info(webserver): HTTP server started");
 }
 
 void setup() {
    delay(1000);
    Serial.begin(9600);
    Serial.println();
-   Serial.print("Configuring access point...");
+   Serial.println("info(ap): configuring access point...");
    
    WiFi.softAP(ssid, password);
-
    IPAddress myIP = WiFi.softAPIP();
-   Serial.print("AP IP address: ");
+   Serial.print("info(ap): Access point successfuly runned; AP IP address ");
    Serial.println(myIP);
-   
-   server.on("/", handleRoot);
-   server.begin();
-   Serial.println("HTTP server started");
+
+   runWebServer();
 }
 
 void loop() {
